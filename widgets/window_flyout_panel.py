bb1c0b9ba0b915af8c2e2ad2a5ef63720a10b6b2
Jed Helmers <jedhelmers@gmail.com>
2023-04-17 23:06:23 -0400

    Update window_flyout_panel.py

--- widgets/window_flyout_panel.py
+++ widgets/window_flyout_panel.py
@@ -8,3 +8,5 @@ class WindowFlyoutPanelWidget(QWidget):
         super().__init__(parent)
         self.ui = window_flyout_panelui.Ui_WindowPanel()
         self.ui.setupUi(self)
+
+        self.setStyleSheet('background: rgba(255, 255, 255, 0.1)')
