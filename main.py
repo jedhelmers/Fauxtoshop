4f73eba24d16911c5fb1b976688ccd93c4452446
jhelmers <jhelmers@miletwo.us>
2023-06-06 10:55:58 -0400

    Update main.py

--- main.py
+++ main.py
@@ -74,23 +74,30 @@ class Tool(QWidget):
         tool = get_tool_icon(self.active_tool)
         print('BUTTS', tool.name)
         if tool.name == "brush":
-            b = QImage(42, 42)
+            size = 600
+            scale = 0.8
+            half_size = (size // 2)
+            scaled_size = half_size ** scale
+            points = scaled_size ** 0.66
+            b = QPixmap(size, size)
             b.fill(Qt.transparent)
             p = QPainter(b)
+            # p.scale(scale, scale)
             p.setRenderHint(QPainter.Antialiasing, True)
             pen = QPen()
-            pen.setColor(Qt.white)
-            p.setCompositionMode(mode_mappings('Screen'))
-            # brush = QBrush(Qt.white)
-            # brush.setColor(Qt.white)
-            # p.setBrush(brush)
-            # p.setBrush(Qt.red)
-            pen.setWidth(1)
+            pen.setColor(QColor(0, 0, 0, 200))
+            # p.setCompositionMode(mode_mappings('Normal'))
+            pen.setWidth(2 / scale)
             p.setPen(pen)
-            p.drawEllipse(QPoint(20, 20), 20, 20)
-
+            p.drawEllipse(QPoint(half_size + 1, half_size + 1), half_size - 4, half_size - 4)
+            print('scaled_size', scaled_size)
             p.end()
-            self.cursor = QtGui.QCursor(b, 27, 13)
+            b = b.scaled(
+                QSize(scaled_size, scaled_size),
+                Qt.IgnoreAspectRatio,
+                Qt.SmoothTransformation
+            )
+            self.cursor = QtGui.QCursor(b, scaled_size // 1.45, points)
         else:
             self.icon = QtGui.QIcon(tool.path).pixmap(QSize(15, 15))
             self.cursor = QtGui.QCursor(self.icon, *tool.hotPoints)
